0bd26e5110d8c67e3f736b40a4716673574539e8
Aleksandr Zhivov <aulifritamin@gmail.com>
2025-09-19 00:06:43 -0700

    added Cube movement

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -2,10 +2,23 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
+    [SerializeField] private Transform cubeTransform;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float scaleSpeed;
 
 
     private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveSpeed = this.moveSpeed * Time.deltaTime;
+
+        cubeTransform.Translate(Vector3.forward * moveSpeed);
+        cubeTransform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        cubeTransform.localScale += new Vector3(scaleSpeed, scaleSpeed, scaleSpeed) * Time.deltaTime;
     }
 }
