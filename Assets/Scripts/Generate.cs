9099b7c8a10c367332728844be93f6e77287fad0
Aleksandr Zhivov <aulifritamin@gmail.com>
2025-09-25 23:53:16 -0700

    make some changes
    make some changes

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -1,33 +1,90 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Generate : MonoBehaviour
 {
+    [Header("Spawn")]
     [SerializeField] private GameObject _prefab;
-    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private int _minChild = 2;
+    [SerializeField] private int _maxChild = 6;
+    [SerializeField] private float _scaleFactor = 0.5f;
+    [SerializeField] private float _spawnJitter = 0.5f;
 
-    private int _min = 2;
-    private int _max = 6;
-    private float _scaleFactor = 0.5f;
+    [Header("Split Chance")]
+    [SerializeField] private float _splitChance = 1f;
+    [SerializeField] private float _childSplitMultiplier = 0.5f;
+    [SerializeField] private float _minScale = 0.1f;
 
-    private Vector3 _parentTransofrm;
-    private Vector3 _scaledTransform;
+    [Header("Explosion Force")]
+    [SerializeField] private float _explosionForce = 300f;
+    [SerializeField] private float _explosionRadius = 10f;
 
     public void Boom()
     {
-        _parentTransofrm = transform.localScale;
-        _scaledTransform = _parentTransofrm * _scaleFactor;
+        float randomSplit = Random.value;
 
-        Destroy(gameObject);
+        Vector3 parentScale = transform.localScale;
+        Vector3 parentPos = transform.position;
+        Vector3 childScale = parentScale * _scaleFactor;
 
-        int randomCount = Random.Range(_min, _max);
-
-        for (int i = 0; i < randomCount; i++)
+        if (transform.localScale.x < _minScale)
         {
-            GameObject newObject = Instantiate(_prefab, transform.position, Quaternion.identity);
-            newObject.transform.localScale = _scaledTransform;
-
-            Renderer rend = GetComponent<Renderer>();
-            rend.material.color = Random.ColorHSV();
+            Destroy(gameObject);
+            return;
         }
+
+        if (randomSplit <= _splitChance)
+        {
+            int randomItemCount = Random.Range(_minChild, _maxChild + 1);
+
+            List<Rigidbody> newCubesRigidbodies = new List<Rigidbody>();
+
+            for (int i = 0; i < randomItemCount; i++)
+            {
+                Vector3 offset = Random.insideUnitSphere * _spawnJitter;
+
+                Rigidbody rb;
+
+                GenerateNewObject(childScale, parentPos, offset, out rb);
+
+                newCubesRigidbodies.Add(rb);
+            }
+
+            ApplyExplosion(newCubesRigidbodies, parentPos);
+
+            Destroy(gameObject);            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void GenerateNewObject(Vector3 childScale, Vector3 parentPos, Vector3 offset, out Rigidbody newRigidbody)
+    {
+        float childSplitChance = _splitChance * _childSplitMultiplier;
+
+        GameObject newObject = Instantiate(_prefab, parentPos, Quaternion.identity);
+
+        newRigidbody = newObject.GetComponent<Rigidbody>();
+        Generate newChance = newObject.GetComponent<Generate>();
+        Renderer newRenderer = newObject.GetComponent<Renderer>();
+
+        newObject.transform.localScale = childScale;
+        newRenderer.material.color = Random.ColorHSV();
+        newChance.SetSplitChance(childSplitChance);     
+    }
+
+    private void ApplyExplosion(List<Rigidbody> rigidBody, Vector3 explosionPosition)
+    {
+        foreach (Rigidbody rb in rigidBody)
+        {
+            rb.AddExplosionForce(_explosionForce, explosionPosition, _explosionRadius);
+        }
+    }
+
+    private void SetSplitChance(float newChance)
+    {
+        _splitChance = newChance;
     }
 }
